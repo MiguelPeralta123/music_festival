36ff86c91e015f6d169da75aae927c7cd5e582f1
Miguel Peralta <maikperalta123@gmail.com>
2023-02-18 20:41:30 -0700

    SASS, Gulp and Normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
+// Gulp is used to automate tasks, sass is used to write better css and gulp-sass is used to connect gulp with sass
 
-const firstTask = ( done ) => {
-    console.log("My first task")
+const { src, dest, watch } = require("gulp")
+const sass = require('gulp-sass')(require('sass'))
+// This dependency prevents the workflow from stopping when an error is found
+const plumber = require('gulp-plumber')
 
-    // This function is a callback to indicate npm that the main function (firstTask) has finished correctly
+// What´s inside .pipe() will execute after the previous pipe had finished executing
+const css = (done) => {
+    // There are three steps to compile a sass file as a css file
+    src('src/scss/**/*.scss') // Identifying sass file
+    .pipe(plumber()) // Using plumber
+    .pipe(sass()) // Compiling it
+    .pipe(dest('build/css')) // Storing it as css
+
+    // Callback that indicates gulp that the execution has finished correctly
     done()
 }
 
-// Here we export the firstTask function. To call it, open a terminal and run "npx gulp task"
-exports.task = firstTask
+const dev = (done) => {
+    // Imported from gulp, this watch function keeps listening to "src/scss/app.scss" waiting for changes to re-execute the css function.
+    // **/* allows us to detect changes in any .scss file inside src/scss folders
+    watch('src/scss/**/*.scss', css)
+    done()
+}
+
+exports.dev = dev
